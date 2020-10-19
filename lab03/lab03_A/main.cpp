e57117427f1fad8379637a8a5a52799424fe6ca7
SMiSSHiN <60625801+SMiSSHiN@users.noreply.github.com>
2020-10-19 03:33:40 +0300

    Update and rename main.cpp to lab03/lab03_A/main.cpp

--- lab03/lab03_A/main.cpp
+++ lab03/lab03_A/main.cpp
@@ -6,66 +6,6 @@
 using namespace Data_Types;
 
 int main(int argc, char*argv[]){
-    /*
-    Vector a; // Вызывается конструктор по-умолчанию
-    Vector b(1, 0); // Если не инициализировать размер, то qsz будет мусор
-    Vector c(5);
-    Vector f(); // Обьявляется ф-ия
-
-    Vector d[5]; // Бужет использоваться конструктор по-умолчанию для каждого элемента массива
-
-    // std::cout << a.SZ << std::endl; // Ошибка, так нельзя
-    a.print(std::cout);
-
-    std::cout << std::endl << std::endl;
-
-    b.print(std::cout);
-
-    std::cout << std::endl << std::endl;
-
-    c.print(std::cout);
-
-    std::cout << std::endl << std::endl;
-
-    d[3].print(std::cout);
-
-    int fuck[] = {4, 9, -2};
-    int cock[100];
-    // Vector e(cock);
-    Vector e(fuck);
-
-    std::cout << std::endl << std::endl;
-
-    e.print(std::cout);
-
-    std::cout << e.max() << std::endl;
-
-    // e.sort().print(std::cout);
-
-    e.add(e.sort()).print(std::cout); // [4, 9, -2] + [-2, 4, 9]
-
-    e.push(100).print(std::cout);
-
-    int vector[20];
-
-    std::cout << e.copy(vector, 1, 3)[3] << std::endl;
-
-    int *ar = e.copy(vector, 1, 3);
-    e.copy(vector, 1, 3);
-
-    for(int i = 0; i < 3; i++)
-        std::cout << ar[i] << ", ";
-    std::cout << std::endl;
-
-    for(int i = 0; i < 20; i ++)
-        std::cout << vector[i] << ", ";
-    std::cout << std::endl;
-
-    Vector x = x.input();
-
-    x.print(std::cout);
-    */
-
     Vector vector(3, -1);
     int fl1, fl2 = 1;
 
